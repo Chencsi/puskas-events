7196d0d989f433e562538cf222fcb4be829f5e54
chencsi <kevinkeeeh@gmail.com>
2023-08-03 11:33:32 +0200

    nincs találat

--- src/App.js
+++ src/App.js
@@ -17,16 +17,14 @@ class App extends React.Component {
     }
 
     render(){
-        const filteredEvents = this.state.events.filter((event) => {
-            return event.eventName
-                .toLowerCase()
-                .includes(this.state.searchfield.toLocaleLowerCase());
-        });
+        const filteredEvent = this.state.events.filter(event =>
+            event.eventName.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        );
         return(
             <div>
                 <h1 class="my-7 text-3xl font-extrabold text-slate-7 00 dark:text-white md:text-5xl lg:text-6xl text-center">Legutóbbi és közelgő események</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <EventList events={filteredEvents} />
+                <EventList events={filteredEvent.length > 0 ? filteredEvent : 404} />
             </div>
         );
     }
--- src/components/event-list.component.jsx
+++ src/components/event-list.component.jsx
@@ -2,28 +2,38 @@ import React from "react";
 import Event from "./event.component";
 
 const EventList = ({ events }) => {
-    const eventComponents = events.map((event) => {
+    if (events === 404) {
         return(
-            <Event
-                key={event.eventId}
-                id={event.eventId}
-                image={event.eventImageSource}
-                name={event.eventName}
-                date={event.eventStartDate}
-                location={event.eventLocation}
-                pay={event.eventHourlyPays}
-                start={event.eventStarts}
-                end={event.eventEnds}
-            />
+            <div className="container rounded-lg py-5 px-5 mx-auto">
+                <h2 className="text-gray-900 dark:text-slate-100 text-center">
+                    {"Nincs találat :("}
+                </h2>
+            </div>
         );
-    });
-    return(
-        <div className="container rounded-lg bg-slate-100 py-3 px-5 mx-auto shadow dark:bg-slate-700">
-            <ul role="list">
-            {eventComponents}
-            </ul>
-        </div>
-    );
+    } else {
+        const eventComponents = events.map((event) => {
+            return(
+                <Event
+                    key={event.eventId}
+                    id={event.eventId}
+                    image={event.eventImageSource}
+                    name={event.eventName}
+                    date={event.eventStartDate}
+                    location={event.eventLocation}
+                    pay={event.eventHourlyPays}
+                    start={event.eventStarts}
+                    end={event.eventEnds}
+                />
+            );
+        });
+        return(
+            <div className="container rounded-lg bg-slate-100 py-3 px-5 mx-auto shadow dark:bg-slate-700">
+                <ul role="list">
+                {eventComponents}
+                </ul>
+            </div>
+        );    
+    }
 }
 
 export default EventList;
